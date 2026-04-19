91b50f7eff21bd703683eadac55d254da608ecfa
Parker Riggs <priggs32304@outlook.com>
2026-04-19 18:57:36 -0400

    Turn off debug mode and fix launch file

--- app.py
+++ app.py
@@ -175,5 +175,4 @@ RULES:
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get("FLASK_ENV") != "production"
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(host="0.0.0.0", port=port, debug=False)
